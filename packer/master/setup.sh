7efcd228bbd9cb730727f3ba84ae0c085c827ac1
PJ <paul@pauljequann.com>
2024-01-16 12:55:28 -0500

    corrected ssh_key naming in setup.sh

--- packer/master/setup.sh
+++ packer/master/setup.sh
@@ -18,9 +18,9 @@ mkdir /var/lib/jenkins/.ssh
 touch /var/lib/jenkins/.ssh/known_hosts
 chown -R jenkins:jenkins /var/lib/jenkins/.ssh
 chmod 700 /var/lib/jenkins/.ssh
-mv /tmp/id_rsa /var/lib/jenkins/.ssh/id_rsa
-chmod 600 /var/lib/jenkins/.ssh/id_rsa
-chown -R jenkins:jenkins /var/lib/jenkins/.ssh/id_rsa
+mv /tmp/id_ed25519 /var/lib/jenkins/.ssh/id_ed25519
+chmod 600 /var/lib/jenkins/.ssh/id_ed25519
+chown -R jenkins:jenkins /var/lib/jenkins/.ssh/id_ed25519
 
 echo "Configure Jenkins"
 mkdir -p /var/lib/jenkins/init.groovy.d
